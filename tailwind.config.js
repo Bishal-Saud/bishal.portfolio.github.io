50c512ccb402dfb21a9be9dc06f240e7f0ad2d96
Bishal-Saud <115933710+Bishal-Saud@users.noreply.github.com>
2023-09-06 16:30:05 +0545

    project html file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
-  theme: {
-    extend: {},
+  purge:{
+    content: ["./*.html","./dist/Projects/*.html"],
   },
+  theme: {
+    extend: { 
+       backgroundColor: {
+      'black': '#141414',
+      'blue-500': '#3333ff',
+    },
+    textColor: {
+      'white': '#eee',
+    },
+    fontFamily: {
+      'poppins': ['Poppins', 'sans'],
+    },
+  }, 
+},
+variants: {},
   plugins: [],
 }
+
+// tailwind.config.js
+
+
 
